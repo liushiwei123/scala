8daf5126b8cce540b6da01c761391b0d23a43eea
liu <2603445803@qq.com>
2019-03-07 00:09:34 +0800

    study scalagit add scala_study/!

---
+++ scala_study/src/main/java/com/study/scala/Test2.java
@@ -0,0 +1,7 @@
+package com.study.scala;
+
+/**
+ * Created by Administrator on 2019/3/6 0006.
+ */
+public class Test2{
+}
---
+++ scala_study/src/main/scala/com/study/scala/Test.scala
@@ -0,0 +1,8 @@
+package com.study.scala
+
+/**
+  * Created by Administrator on 2019/3/6 0006.
+  */
+object Test {
+
+}
