f3b9a40bf4692fbf587ec9543be16ef4aa4ad39c
liu <2603445803@qq.com>
2019-03-07 00:28:55 +0800

    test commit

--- scala_study/src/main/java/com/study/scala/char01/TestJava.java
+++ scala_study/src/main/java/com/study/scala/char01/TestJava.java
@@ -4,4 +4,7 @@ package com.study.scala.char01;
  * Created by Administrator on 2019/3/7 0007.
  */
 public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("test git");
+    }
 }
